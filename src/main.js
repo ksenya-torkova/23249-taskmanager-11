8440f85855d45cd84e21361d656c5ad4cec6a683
ksenya <ksenya.torkova.stratis@gmail.com>
2020-04-18 19:54:43 +0300

    дорабатывает компоненты по заданию "Революция или эволюция"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import {render} from './utils.js';
-// import LoadMoreComponent from './components/load-more.js';
-// import TaskEditComponent from './components/task-edit.js';
 import SiteMenuComponent from './components/site-menu.js';
+import TaskListComponent from './components/task-list.js';
+// import TaskEditComponent from './components/task-edit.js';
+// import LoadMoreComponent from './components/load-more.js';
+// import NoTaskComponent from './components/no-tasks.js';
 import FilterComponent from './components/filter.js';
 import BoardComponent from './components/board.js';
 import TaskComponent from './components/task.js';
+import SortComponent from './components/sort.js';
 import {generateFilters} from './mock/filter-mock.js';
 import {generateTasksList} from './mock/task-mock.js';
 
@@ -35,20 +38,24 @@ render(siteMain, new FilterComponent(filters).getElement());
 //   }
 // });
 
-const renderTask = (taskListContainer, task) => {
+const renderTask = (tasksContainer, task) => {
   const taskComponent = new TaskComponent(task);
   // const taskEditComponent = new TaskEditComponent(task);
 
-  render(taskListContainer, taskComponent.getElement());
+  render(tasksContainer, taskComponent.getElement());
 };
 
 const renderBoard = (tasks, boardComponent) => {
-  const taskList = boardComponent.getElement().querySelector(`.board__tasks`);
+  const taskListComponent = new TaskListComponent().getElement();
+
+  render(boardComponent.getElement(), new SortComponent().getElement());
+  render(boardComponent.getElement(), taskListComponent);
+
   let showingTasksAmount = DEFAULT_TASKS_AMOUNT;
 
   tasks.slice(0, showingTasksAmount)
     .forEach((task) => {
-      renderTask(taskList, task);
+      renderTask(taskListComponent, task);
     });
 };
 
